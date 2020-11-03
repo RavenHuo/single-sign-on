e8fcba5bdc755c3363bd3513ea61702aed6e02ed
raven <huoruiwen@rongshantech.com>
2020-11-03 22:09:56 +0800

    定义服务

---
+++ sso-springboot/src/main/java/com/raven/sso/springboot/SsoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.raven.sso.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoSpringbootApplication.class, args);
+    }
+
+}
---
+++ sso-springboot/src/test/java/com/raven/sso/springboot/SsoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raven.sso.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
