ffc28304da31d8eecd2de4fe176b02662777d22b
raven <huoruiwen@rongshantech.com>
2020-11-06 21:48:52 +0800

    sso-zuul

---
+++ sso-springcloud-zuul/src/main/java/com/raven/sso/zuul/SsoSpringcloudZuulApplication.java
@@ -0,0 +1,13 @@
+package com.raven.sso.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoSpringcloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoSpringcloudZuulApplication.class, args);
+    }
+
+}
